80df8270116fc2a57567933b0decbd2403d1c62a
Malcolm <malcolmbalch@gmail.com>
2020-01-11 01:18:24 -0500

    Remove old draw_fps function

--- demos/endurance/src/main.rs
+++ demos/endurance/src/main.rs
@@ -126,23 +126,3 @@ fn main() -> Result<(), String> {
 
     Ok(())
 }
-
-fn draw_fps(canvas: &mut WindowCanvas, fps: f32) {
-    let to_draw = format!("FPS: {}", fps as u32);
-    // println!("{}", to_draw);
-    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
-    let texture_creator = canvas.texture_creator();
-    let font_path = "/home/malcolm/Downloads/RobotoCondensed-Bold.ttf";
-    let mut font = ttf_context.load_font(font_path, 60).unwrap();
-    font.set_style(sdl2::ttf::FontStyle::BOLD);
-    let surface = font.render(&to_draw)
-        .blended(Color::RGBA(0, 0, 0, 255)).map_err(|e| e.to_string()).unwrap();
-    let texture = texture_creator.create_texture_from_surface(&surface)
-        .map_err(|e| e.to_string()).unwrap();
-
-    let TextureQuery { width, height, .. } = texture.query();
-
-    let target = Rect::new(15, 0, width, height);
-
-    canvas.copy(&texture, None, Some(target)).unwrap();
-}
